2e766608131b5d02b25c8c5444a026a890fd7567
codejay411 <jaypr202@gmail.com>
2022-01-19 16:17:52 +0530

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 # save this as app.py
 from flask import Flask, escape, request, render_template
+import os
+import numpy as np
+import tensorflow as tf
+from keras.models import load_model
+from keras.preprocessing import image
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# model = load model("models/fruits.h5")
+model = load_model("models/1")
+
+class_name = ['freshapples',
+ 'freshbanana',
+ 'freshoranges',
+ 'rottenapples',
+ 'rottenbanana',
+ 'rottenoranges']
 
 app = Flask(__name__)
 
@@ -15,9 +32,31 @@ def about():
 def contact():
     return render_template("contact.html")
 
-@app.route('/prediction')
+@app.route('/prediction', methods=['GET', 'POST'])
 def prediction():
-    return render_template("prediction.html")
+    if request.method == 'POST':
+        f = request.files['fruit']
+        filename= f.filename
+        target = os.path.join(APP_ROOT, 'images/')
+        print(target)
+        des = "/".join([target, filename])
+        f.save(des)
+
+        test_image= image.load_img('images\\'+filename,target_size=(300,300))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        prediction = model.predict(test_image)
+        print(prediction)
+
+        predicted_class= class_name[np.argmax(prediction[0])]
+        print(predicted_class)
+        confidence = round(np.max(prediction[0])*100)
+        print(confidence)
+
+        return render_template("prediction.html", confidence= "chances -> "+str(confidence)+ "%", prediction = "prediction -> "+str(predicted_class))
+
+    else:
+        return render_template("prediction.html")
 
 
 
